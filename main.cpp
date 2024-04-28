e2f3bac8e07fa69914361385bc33fb4fe5d998ad
Viktoryia <manuelka.wikta@gmail.com>
2024-04-28 11:18:03 +0200

    Revert "add cosmetic changes"
    This reverts commit 336dad5d77e3660cd66bf0c43fedb0faef66ac4d.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
 #include "rbtree.h"
 
-int main()
-{
+int main() {
 
 	RedBlackTree<double> mytree;
 	std::cout << "RED-BLACK TREE IMPLEMENTATION\n";
 	std::cout << "\n\nTREE AFTER INSERTION THE ELEMENTS:\n\n";
-
+	
 	mytree.insert(10);
 	mytree.insert(18);
 	mytree.insert(7);
 	mytree.insert(15);
 	mytree.insert(16);
 	mytree.insert(30);
-	mytree.insert(6);
-	mytree.insert(13);
+    mytree.insert(6);
+    mytree.insert(13);
 
 	mytree.pretty_print();
 
-	mytree.delete_node(7);
+
+    mytree.delete_node(7);
 	std::cout << "\n\n...meanwhile I deleted 7...\n";
 	mytree.delete_node(13);
 	std::cout << "...meanwhile I deleted 13...\n";
 	mytree.delete_node(6);
 	std::cout << "...meanwhile I deleted 6...\n";
 	std::cout << "\n\nTREE AFTER DELETION THE ELEMENTS:\n\n";
-	mytree.pretty_print();
+    mytree.pretty_print();
 
 	std::cout << "\ninorder :\n";
 	mytree.inorder();
